28efc6912cc7d4604e42017c4fa45ccb6e71c4e9
Akash-Mittal <mittal.akash.adam@gmail.com>
2018-10-26 09:52:47 +0530

    Changes:1.) Added Application.

---
+++ templates/parent/parent-child-one/src/main/java/com/am/innovations/child/one/Application.java
@@ -0,0 +1,10 @@
+package com.am.innovations.child.one;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
