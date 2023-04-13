dd726d08bdddc208b001fd0cf7ce4d7ee54ad0f0
Akash Mittal <mittal.akash.adam@gmail.com>
2023-04-13 11:06:55 +0200

    Changes:1.) Adjusted to Each timezone

---
+++ etc/digital-clock-template/command.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Nick Korotysh\Digital Clock\digital_clock.exe" europe-clock_settings-mauve.dcs
+"C:\Program Files\Nick Korotysh\Digital Clock\digital_clock.exe" india-clock_settings-red.dcs
+
